62e25f6b4b6e9b1d5bc23b59a46dca8e3a4e8438
cuki2 <mauricio@krebs.com.br>
2018-02-20 16:11:41 -0300

    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- GraphicView/GraphicView.pro
+++ GraphicView/GraphicView.pro
@@ -5,6 +5,7 @@
 #-------------------------------------------------
 
 QT       += core gui
+QT += charts
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -24,13 +25,8 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 
 SOURCES += \
-        main.cpp \
-        mainwindow.cpp \
-    pizza.cpp
+        main.cpp
 
-HEADERS += \
-        mainwindow.h \
-    pizza.h
+HEADERS +=
 
-FORMS += \
-        mainwindow.ui
+FORMS +=
--- GraphicView/main.cpp
+++ GraphicView/main.cpp
@@ -1,11 +1,42 @@
-#include "mainwindow.h"
-#include <QApplication>
+//https://doc.qt.io/qt-5.10/qtcharts-piechart-example.html
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
+
+QT_CHARTS_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QPieSeries *series = new QPieSeries();
+    series->append("Jane", 1);
+    series->append("Joe", 2);
+    series->append("Andy", 3);
+    series->append("Barbara", 4);
+    series->append("Axel", 5);
+
+    QPieSlice *slice = series->slices().at(4);
+    slice->setExploded();
+    slice->setLabelVisible();
+    slice->setPen(QPen(Qt::darkGreen, 2));
+    slice->setBrush(Qt::green);
+
+    QChart *chart = new QChart();
+    chart->addSeries(series);
+    chart->setTitle("Simple piechart example");
+    chart->legend()->hide();
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QMainWindow window;
+    window.setCentralWidget(chartView);
+    window.resize(400, 300);
+    window.show();
 
     return a.exec();
 }
