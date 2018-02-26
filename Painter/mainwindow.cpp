3f1e527382a6c06f50fdcbc143e5eb11000c7498
maurice <mauricio@krebs.com.br>
2018-02-26 11:53:06 -0300

    Signed-off-by: maurice <mauricio@krebs.com.br>

--- Painter/mainwindow.cpp
+++ Painter/mainwindow.cpp
@@ -14,13 +14,14 @@ MainWindow::MainWindow(QWidget *parent) :
     angulo = 0;
 
     QPen pen;
-    //    pen.setStyle(Qt::DashDotLine);
+    pen.setStyle(Qt::DashDotLine);
     pen.setWidth(3);
-    //    pen.setBrush(Qt::green);
+    pen.setBrush(Qt::green);
     pen.setCapStyle(Qt::RoundCap);
     pen.setJoinStyle(Qt::RoundJoin);
 
-    painter = new Pizza(this);
+//    painter = new Pizza(this);
+    painter = new Pizza(ui->graphicsView->viewport());
     painter->setaRaio(600);
     painter->setaAngulo(0);
     painter->setPen(pen);
@@ -33,6 +34,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow()
 {
+    if (timer)
+        timer->stop();
+
+    delete timer;
     delete painter;
     delete ui;
 }
@@ -42,13 +47,29 @@ void MainWindow::paintEvent(QPaintEvent *)
     if (!painter)
         return;
 
+    //    QPen pen;
+    //        pen.setStyle(Qt::DashDotLine);
+    //    pen.setWidth(3);
+    //        pen.setBrush(Qt::green);
+    //    pen.setCapStyle(Qt::RoundCap);
+    //    pen.setJoinStyle(Qt::RoundJoin);
+
+    //    painter->setaAngulo(angulo);
+    //    painter->desenhaPosicao();
+    //    Pizza painter(this);
+    //    Pizza painter(ui->graphicsView->viewport());
+    //    painter.setaRaio(600);
+    //    painter.setPen(pen);
+    //    painter.setaAngulo(angulo);
+    //    painter.end();
+    //    painter.desenhaPosicao();
     painter->setaAngulo(angulo);
     painter->desenhaPosicao();
 
     angulo += 0.25;
 
-    if (angulo >= 360.0)
+    if (angulo >= 360.0 || angulo < 0.0)
         angulo = 0.0;
 
-    qDebug() << "Paint";
+    qDebug() << "Paint " << angulo;
 }
