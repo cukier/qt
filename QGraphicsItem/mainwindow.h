6af4c81669d921f1036557bed97fbf5142aa2294
maurice <mauricio@krebs.com.br>
2018-02-27 16:48:33 -0300

    Signed-off-by: maurice <mauricio@krebs.com.br>

---
+++ QGraphicsItem/mainwindow.h
@@ -0,0 +1,24 @@
+//http://www.bogotobogo.com/Qt/Qt5_QGraphicsView_QGraphicsScene_QGraphicsItems.php
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
---
+++ QGraphicsItem/pizza.h
@@ -0,0 +1,12 @@
+#ifndef PIZZA_H
+#define PIZZA_H
+
+#include <QGraphicsItem>
+
+class Pizza : public QGraphicsItem
+{
+public:
+    Pizza();
+};
+
+#endif // PIZZA_H
